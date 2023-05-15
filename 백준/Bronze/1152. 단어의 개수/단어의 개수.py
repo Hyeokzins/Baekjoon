ca2989453dde7a814e289b8687bf657f99053e90
Hyeokzins <128476961+Hyeokzins@users.noreply.github.com>
2023-05-15 10:27:36 +0900

    [Bronze II] Title: 단어의 개수, Time: 52 ms, Memory: 39196 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,14 +1,5 @@
-a=input().split(' ')
-k=len(a)
-
-if a[0]=='':
-    k-=1
-    if a[-1]=='':
-        k-=1
-elif a[-1]=='':
-    k-=1
-    if a[0]=='':
-        k-=1
+b=input().split()
+k=len(b)
 
 print(k)
 
