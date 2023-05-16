efd0358d4eb4623985c2b7b364a683dc5b843a6e
Hyeokzins <128476961+Hyeokzins@users.noreply.github.com>
2023-05-16 13:27:55 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 50420 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,28 +1,11 @@
-import sys
-N=int(sys.stdin.readline().rstrip())
-A=[x for x in map(int,list(sys.stdin.readline().rstrip().split()))]
-A.sort()
+N=int(input())
+A=set(map(int, input().split()))
 
-M=int(sys.stdin.readline().rstrip())
-B=[x for x in map(int,list(sys.stdin.readline().rstrip().split()))]
-B_sort=sorted(B)
-       
-def detect(i):
-    target=B[i]
-    left=0
-    right=N-1
-    answer=0
+M=int(input())
+B=[x for x in map(int,input().split())]
 
-    while left<=right:
-        mid=(left+right)//2
-        if A[mid]==target:
-            answer=1
-            break
-        elif A[mid]>target:
-            right=mid-1
-        else:
-            left=mid+1
-    print(answer)
-
-for i in range(M):
-    detect(i)
+for i in B:
+    if i in A:
+        print('1')
+    else:
+        print('0')
